3bc048dfb7ccd676decba2027567a3bbcbc60f44
anton-voron <anton.voron.gt@gmail.com>
2019-07-23 23:37:45 +0300

    Updates

---
+++ precache-manifest.054f6e86df2bd0d86b3cc9b1222fdb39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc848be59e113f3308efeeeec21c210",
+    "url": "/auth-app/index.html"
+  },
+  {
+    "revision": "7dd650d17116967f652c",
+    "url": "/auth-app/static/css/main.4dc3c9f6.chunk.css"
+  },
+  {
+    "revision": "c70774d8fec55627329a",
+    "url": "/auth-app/static/js/2.95fd7be4.chunk.js"
+  },
+  {
+    "revision": "7dd650d17116967f652c",
+    "url": "/auth-app/static/js/main.5fc4f915.chunk.js"
+  },
+  {
+    "revision": "226c33d0a60e8d3afc71",
+    "url": "/auth-app/static/js/runtime~main.bf9c4ad0.js"
+  }
+]);
